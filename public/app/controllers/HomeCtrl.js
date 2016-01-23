2e8f120871c4cc8da6f6235ac4951f2b9edd49a5
Kevin Amiranoff <kamiranoff@gmail.com>
2016-01-23 18:15:12 +0100

    adds basic post-it with socket connection

--- public/app/controllers/HomeCtrl.js
+++ public/app/controllers/HomeCtrl.js
@@ -1,21 +1,38 @@
 'use strict';
 
-app.controller('HomeCtrl', function($scope, $location){
-	var socket = io();
+app.controller('HomeCtrl', function($scope, socket) {
+  console.log('on est dans le HomeCtrl');
 
-	console.log('on est dans le HomeCtrl');
 
-	$scope.createPostIt = function(){
-		console.log("on crée uin postIt");
-		socket.emit('create', {
-			name  : "pouet",
-			creator : "Angular",
-			content : []
-		});
-	}
 
-	socket.on('postItArray', function(data){
-		console.log(data);
-	});
+  $scope.createPostIt = function() {
+    console.log("on crée uin postIt");
+    socket.emit('create', {
+      name: "pouet",
+      creator: "Angular",
+      content: []
+    });
+  }
+
+  socket.on('postItArray', function(data) {
+    console.log(data);
+  });
+
+
+
+
+
+  $scope.submitPostit = function() {
+    console.log('clicked');
+    socket.emit('submitPostit', $scope.postitTitle);
+
+    return false;
+  }
+
+  socket.on('submitPostit', function(msg) {
+    console.log(msg);
+    $scope.postitTitle = msg.data;
+
+  });
+
 });
-
--- server.js
+++ server.js
@@ -6,4 +6,20 @@ var	io      = require('socket.io').listen(server);
 
 require('./server/chatServer.js')(io);
 
+<<<<<<< ea97be0ae5b0cf255f60c3d52875616445b45ad8
 app.use(express.static(__dirname +'/public'));
+||||||| merged common ancestors
+// handles socket stuff
+io.sockets.on('connection', function (socket) 
+{
+	console.log("user logged");
+});
+=======
+// handles socket stuff
+io.sockets.on('connection', function (socket)
+{
+  socket.on('submitPostit',function(data){
+    socket.broadcast.emit('submitPostit',{data:data});
+  });
+});
+>>>>>>> adds basic post-it with socket connection
